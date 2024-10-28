05520089f974211abd759da11d4e31f1e7aec2a3
lucasverdelho <PG50221@alunos.uminho.pt>
2024-10-28 19:35:04 +0000

    corrected process output

--- face_recognition.py
+++ face_recognition.py
@@ -15,7 +15,6 @@ class FaceRecognition:
             print(f"Error reading - {os.path.basename(file_path)}")
             return []
         
-        original_height, original_width = img.shape[:2] 
         faces = self.app.get(img) 
         
         if len(faces) == 0:
@@ -24,31 +23,19 @@ class FaceRecognition:
         
         embeddings_data = []
         
-        for idx, face in enumerate(faces):
+        for face_id, face in enumerate(faces):
 
             x1, y1, x2, y2 = face.bbox.astype(int)
 
             center_x = (x1 + x2) // 2
             center_y = (y1 + y2) // 2
-
-            # Testing to see if coordinates are correct
-            # cv2.drawMarker(img, (center_x, center_y), color=(0, 0, 255), 
-            #             markerType=cv2.MARKER_CROSS, markerSize=20, thickness=2)
-
-
-            print(f"Bounding Box for Face {idx + 1} in {os.path.basename(file_path)}: "
+            
+            print(f"Bounding Box for Face {face_id + 1} in {os.path.basename(file_path)}: "
                 f"({x1}, {y1}) to ({x2}, {y2})")
             print(f"Calculated Center: ({center_x}, {center_y})")
 
-            # Store the embeddings and coordinates
-            embeddings_data.append((file_path, idx, face.normed_embedding, (center_x, center_y)))
-
-
-        # testing only
-        # marked_image_path = f"marked_{os.path.basename(file_path)}"
-        # cv2.imwrite(marked_image_path, img)
-        # print(f"Marked image saved as: {marked_image_path}")
-    
+            coordinates = [center_x, center_y]
+            embeddings_data.append((file_path, face.normed_embedding, coordinates, face_id))
 
         return embeddings_data
 
@@ -68,20 +55,6 @@ class FaceRecognition:
                 face_data.extend(face_embeddings)
         
         return face_data
-
-    # Temporarily storing all face data locally just for testing the clustering
-    def save_to_csv(self, face_data, csv_file='face_data.csv'):
-        with open(csv_file, mode='w', newline='') as file:
-            writer = csv.writer(file, delimiter=';')
-            writer.writerow(["media_Id", "embedding", "coordinates"]) 
-            
-            for file_path, face_idx, embedding, (center_x, center_y) in face_data:
-                embedding_list = embedding.tolist()
-                coordinates = f"({center_x}, {center_y})"
-                writer.writerow([os.path.basename(file_path), embedding_list, coordinates])
-
-
-        print(f"Data saved to {csv_file}")
 
 
     def compare_all_faces(self, face_data):
