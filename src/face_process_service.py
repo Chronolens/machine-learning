a35a940c149cd16d10dd360f38d13c7a58adba1e
lucasverdelho <PG50221@alunos.uminho.pt>
2024-11-06 10:28:55 +0000

    todo updated

--- src/face_process_service.py
+++ src/face_process_service.py
@@ -169,3 +169,6 @@ async def main():
 if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO)
     asyncio.run(main())
+
+
+# TODO: Insert bounding box as well as coordinates in the database
